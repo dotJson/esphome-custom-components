e3072d0025ffb2e14fe39aa8e37ea2ccd53cd0ba
Jason Young <jason@sdyoung.com>
2026-09-03 20:12:06 -0700

    Refactor TMC2209Stepper class for clarity and style

--- esphome/components/tmc2209/stepper/tmc2209_stepper.h
+++ esphome/components/tmc2209/stepper/tmc2209_stepper.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "esphome/components/tmc2209/tmc2209_api_registers.h"
 #include "esphome/components/tmc2209/tmc2209_api.h"
 #include "esphome/components/tmc2209/tmc2209_component.h"
@@ -23,15 +24,18 @@ enum ControlMethod {
 struct IndexPulseStore {
   int32_t *current_position_ptr{nullptr};
   Direction *direction_ptr{nullptr};
+
   static void IRAM_ATTR HOT pulse_isr(IndexPulseStore *arg) {
-    (*(arg->current_position_ptr)) += (int8_t) (*(arg->direction_ptr));
+    (*(arg->current_position_ptr)) +=
+        (int8_t) (*(arg->direction_ptr));
   }
 };
 
 class TMC2209Stepper : public TMC2209Component, public Stepper {
  public:
   TMC2209Stepper() = default;
-  TMC2209Stepper(uint8_t address) : TMC2209Component(address){};
+  TMC2209Stepper(uint8_t address)
+      : TMC2209Component(address){};
 
   void dump_config() override;
   void setup() override;
@@ -39,26 +43,100 @@ class TMC2209Stepper : public TMC2209Component, public Stepper {
   void on_shutdown() override;
   void stop() override;
   void enable(bool enable) override;
+
   // void enable(bool enable, bool recover_toff = true) override;
+
   void set_target(int32_t steps) override;
   bool is_stalled() override;
 
-  void set_control_method(ControlMethod method) { this->control_method_ = method; }
+  void set_control_method(ControlMethod method) {
+    this->control_method_ = method;
+  }
 
  protected:
   HighFrequencyLoopRequester high_freq_;
-  ControlMethod control_method_{ControlMethod::CONTROL_UNSET};
+  ControlMethod control_method_{
+      ControlMethod::CONTROL_UNSET};
 
   /** Serial control */
-  IndexPulseStore ips_;  // index pulse store
+  IndexPulseStore ips_;
   int32_t vactual_ = 0;
-  /* */
+  /**/
 
   /** Pulses control */
-  bool step_state_ = false;
-  Direction direction_;
-  /* */
+  bool step_state_{false};
+
+  // Explicit initialization avoids an undefined direction state
+  // before the first commanded move.
+  Direction direction_{Direction::STANDSTILL};
+  /**/
 };
 
 }  // namespace tmc2209
 }  // namespace esphome
+
+
+// #pragma once
+// #include "esphome/components/tmc2209/tmc2209_api_registers.h"
+// #include "esphome/components/tmc2209/tmc2209_api.h"
+// #include "esphome/components/tmc2209/tmc2209_component.h"
+// #include "esphome/components/tmc2209/events.h"
+
+// #include "esphome/core/helpers.h"
+// #include "esphome/core/component.h"
+// #include "esphome/core/automation.h"
+// #include "esphome/components/stepper/stepper.h"
+
+// namespace esphome {
+// namespace tmc2209 {
+
+// using namespace esphome::stepper;
+
+// enum ControlMethod {
+//   CONTROL_UNSET,
+//   SERIAL_CONTROL,
+//   PULSES_CONTROL,
+// };
+
+// struct IndexPulseStore {
+//   int32_t *current_position_ptr{nullptr};
+//   Direction *direction_ptr{nullptr};
+//   static void IRAM_ATTR HOT pulse_isr(IndexPulseStore *arg) {
+//     (*(arg->current_position_ptr)) += (int8_t) (*(arg->direction_ptr));
+//   }
+// };
+
+// class TMC2209Stepper : public TMC2209Component, public Stepper {
+//  public:
+//   TMC2209Stepper() = default;
+//   TMC2209Stepper(uint8_t address) : TMC2209Component(address){};
+
+//   void dump_config() override;
+//   void setup() override;
+//   void loop() override;
+//   void on_shutdown() override;
+//   void stop() override;
+//   void enable(bool enable) override;
+//   // void enable(bool enable, bool recover_toff = true) override;
+//   void set_target(int32_t steps) override;
+//   bool is_stalled() override;
+
+//   void set_control_method(ControlMethod method) { this->control_method_ = method; }
+
+//  protected:
+//   HighFrequencyLoopRequester high_freq_;
+//   ControlMethod control_method_{ControlMethod::CONTROL_UNSET};
+
+//   /** Serial control */
+//   IndexPulseStore ips_;  // index pulse store
+//   int32_t vactual_ = 0;
+//   /* */
+
+//   /** Pulses control */
+//   bool step_state_ = false;
+//   Direction direction_;
+//   /* */
+// };
+
+// }  // namespace tmc2209
+// }  // namespace esphome
