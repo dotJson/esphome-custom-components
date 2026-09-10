ece83bed364155253312e8861a6598ee7041a178
Jason Young <jason@sdyoung.com>
2026-09-10 11:08:34 -0700

    Enable HTTP client dependency for blinds_common

--- esphome/components/blinds_common/__init__.py
+++ esphome/components/blinds_common/__init__.py
@@ -1,5 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components.esp32 import include_builtin_idf_component
 
 CODEOWNERS = ["@dotJson"]
 
@@ -7,6 +8,11 @@ CONFIG_SCHEMA = cv.Schema({})
 
 
 async def to_code(config):
+    # ESPHome 2026.2+ excludes the ESP-IDF HTTP client unless a component
+    # explicitly requests it. webhook_transport.cpp uses esp_http_client.h.
+    include_builtin_idf_component("esp_http_client")
+
+    # Expose the component's public interfaces to generated YAML lambdas.
     cg.add_global(cg.RawStatement('#include "esphome/components/blinds_common/persistent_settings.h"'))
     cg.add_global(cg.RawStatement('#include "esphome/components/blinds_common/activity_events.h"'))
     cg.add_global(cg.RawStatement('#include "esphome/components/blinds_common/webhook_transport.h"'))
